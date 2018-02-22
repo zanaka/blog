fede8341e3856b0c3fd3697dbfe5e5ab1b15626c
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-22 22:02:53 +0900

    fix

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,21 @@ use Slim\Http\Response;
 
 // Routes
 
+//固定パス
+//$app->get('/hello', function ($request, $response, $args) {
+//    echo 'Hello,world!';
+//});
+
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
