7a120c28cbdb98eee8ef73fdcfe17e4bb16f316d
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-27 19:38:31 +0900

    CRUDの処理を追加

--- src/routes.php
+++ src/routes.php
@@ -1,25 +1,79 @@
 <?php
-
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
 
-//固定パス
-//$app->get('/hello', function ($request, $response, $args) {
-//    echo 'Hello,world!';
-//});
+// 一覧表示
+$app->get('/tickets', function (Request $request, Response $response) {
+    return $response->write('tickets');
+});
 
+// 新規作成用フォームの表示
+$app->get('/tickets/create', function (Request $request, Response $response) {
+    $subject = $request->getParsedBodyParam('subject');
+    return $this->renderer->render($response, 'tasks/create.phtml');
+});
+
+// 新規作成
+$app->post('/tickets', function (Request $request, Response $response) {
+    $subject = $request->getParsedBodyParam('subject');
+    // ここに保存の処理を書く
+    $sql = 'INSERT INTO tickets (subject) values (:subject)';
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(['subject' => $subject]);
+    if (!$result) {
+        throw new \Exception('could not save the ticket');
+    }
+
+    // 保存が正常にできたら一覧ページへリダイレクトする
+    return $response->withRedirect("/tickets");
+});
+
+// 表示
+$app->get('/tickets/{id}', function (Request $request, Response $response, array $args) {
+});
+
+// 編集用フォームの表示
+$app->get('/tickets/{id}/edit', function (Request $request, Response $response, array $args) {
+});
+
+// 更新
+$app->put('/tickets/{id}', function (Request $request, Response $response, array $args) {
+});
+
+// 削除
+$app->delete('/tickets/{id}', function (Request $request, Response $response, array $args) {
+});
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
-
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+//
+//use Slim\Http\Request;
+//use Slim\Http\Response;
+//
+//// Routes
+//
+////固定パス
+////$app->get('/hello', function ($request, $response, $args) {
+////    echo 'Hello,world!';
+////});
+//
+//
+//$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+//    // Sample log message
+//    $this->logger->info("Slim-Skeleton '/' route");
+//
+//    // Render index view
+//    return $this->renderer->render($response, 'index.phtml', $args);
+//});
+//
+//
+//$app->get('/hello/{name}', function ($request, $response, $args) {
+//    return $this->renderer->render($response, 'index.phtml', $args);
+//});
