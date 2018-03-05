bcc65f894dbd04d9b7ffe845db7420f57f2e8d26
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-03-05 12:45:53 +0900

    編集・更新・削除のルーティングと処理を追記

--- src/routes.php
+++ src/routes.php
@@ -52,14 +52,44 @@ $app->get('/tickets/{id}', function (Request $request, Response $response, array
 
 // 編集用フォームの表示
 $app->get('/tickets/{id}/edit', function (Request $request, Response $response, array $args) {
+    $sql = 'SELECT * FROM tickets WHERE id = :id';
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(['id' => $args['id']]);
+    $ticket = $stmt->fetch();
+    if (!$ticket) {
+        return $response->withStatus(404)->write('not found');
+    }
+    $data = ['ticket' => $ticket];
+    return $this->renderer->render($response, 'tickets/edit.phtml', $data);
 });
 
 // 更新
 $app->put('/tickets/{id}', function (Request $request, Response $response, array $args) {
+    $sql = 'SELECT * FROM tickets WHERE id = :id';
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(['id' => $args['id']]);
+    $ticket = $stmt->fetch();
+    if (!$ticket) {
+        return $response->withStatus(404)->write('not found');
+    }
+    $ticket['subject'] = $request->getParsedBodyParam('subject');
+    $stmt = $this->db->prepare('UPDATE tickets SET subject = :subject WHERE id = :id');
+    $stmt->execute($ticket);
+    return $response->withRedirect("/tickets");
 });
 
 // 削除
 $app->delete('/tickets/{id}', function (Request $request, Response $response, array $args) {
+    $sql = 'SELECT * FROM tickets WHERE id = :id';
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(['id' => $args['id']]);
+    $ticket = $stmt->fetch();
+    if (!$ticket) {
+        return $response->withStatus(404)->write('not found');
+    }
+    $stmt = $this->db->prepare('DELETE FROM tickets WHERE id = :id');
+    $stmt->execute(['id' => $ticket['id']]);
+    return $response->withRedirect("/tickets");
 });
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
