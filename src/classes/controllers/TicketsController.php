1cd5d3956056927c754e246aab8d7103c060515d
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-03-09 15:58:14 +0900

    TicketsControllerクラスの作成

---
+++ src/classes/controllers/TicketsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Classes\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class TicketsController extends Controller
+{
+    public function index(Request $request, Response $response)
+    {
+    }
+}
