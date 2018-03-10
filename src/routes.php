7b142bdc603d53c0aad5e607162d4a0b93d692a0
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-03-10 17:18:26 +0900

    routesをticketsControllerに移動

--- src/routes.php
+++ src/routes.php
@@ -1,103 +1,33 @@
 <?php
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Classes\Controllers\TicketsController;
 
 // Routes
 
-// トップページの表示
-$app->get('/', function (Request $request, Response $response) {
-    return $this->renderer->render($response, 'home/top.phtml');
-});
+//// トップページの表示
+//$app->get('/', function (Request $request, Response $response) {
+//    return $this->renderer->render($response, 'home/top.phtml');
+//});
 
 // 一覧表示
-$app->get('/tickets', function (Request $request, Response $response) {
-    $sql = 'SELECT * FROM tickets';
-    $stmt = $this->db->query($sql);
-    $tickets = [];
-    while($row = $stmt->fetch()) {
-        $tickets[] = $row;
-    }
-    $data = ['tickets' => $tickets];
-    return $this->renderer->render($response, 'tickets/index.phtml', $data);
-});
-
+$app->get('/tickets', TicketsController::class . ':index');
 // 新規作成用フォームの表示
-$app->get('/tickets/create', function (Request $request, Response $response) {
-    return $this->renderer->render($response, 'tickets/create.phtml');
-});
-
+$app->get('/tickets/create', TicketsController::class . ':create');
 // 新規作成
-$app->post('/tickets', function (Request $request, Response $response) {
-    $title = $request->getParsedBodyParam('title');
-    $contents = $request->getParsedBodyParam('contents');
-
-    // ここに保存の処理を書く
-    $sql = 'INSERT INTO tickets (title,contents) values (:title,:contents)';
-
-    // コンテナに登録したPDOのオブジェクトは$this->dbでアクセスできる
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(['title' => $title, 'contents' => $contents]);
-    if (!$result) {
-        throw new \Exception('could not save the ticket');
-    }
-
-    // 保存が正常にできたら一覧ページへリダイレクトする
-    return $response->withRedirect("/tickets");
-});
-
+$app->post('/tickets', TicketsController::class . ':store');
 // 表示
-$app->get('/tickets/{id}', function (Request $request, Response $response, array $args) {
-    $sql = 'SELECT * FROM tickets WHERE id = :id';
-    $stmt = $this->db->prepare($sql);
-    $stmt->execute(['id' => $args['id']]);
-    $ticket = $stmt->fetch();
-    if (!$ticket) {
-        return $response->withStatus(404)->write('not found');
-    }
-    $data = ['ticket' => $ticket];
-    return $this->renderer->render($response, 'tickets/show.phtml', $data);
-});
-
+$app->get('/tickets/{id}', TicketsController::class . ':show');
 // 編集用フォームの表示
-$app->get('/tickets/{id}/edit', function (Request $request, Response $response, array $args) {
-    $sql = 'SELECT * FROM tickets WHERE id = :id';
-    $stmt = $this->db->prepare($sql);
-    $stmt->execute(['id' => $args['id']]);
-    $ticket = $stmt->fetch();
-    if (!$ticket) {
-        return $response->withStatus(404)->write('not found');
-    }
-    $data = ['ticket' => $ticket];
-    return $this->renderer->render($response, 'tickets/edit.phtml', $data);
-});
-
+$app->get('/tickets/{id}/edit', TicketsController::class . ':edit');
 // 更新
-$app->put('/tickets/{id}', function (Request $request, Response $response, array $args) {
-    $sql = 'SELECT * FROM tickets WHERE id = :id';
-    $stmt = $this->db->prepare($sql);
-    $stmt->execute(['id' => $args['id']]);
-    $ticket = $stmt->fetch();
-    if (!$ticket) {
-        return $response->withStatus(404)->write('not found');
-    }
-    $ticket['title'] = $request->getParsedBodyParam('title');
-    $ticket['contents'] = $request->getParsedBodyParam('contents');
-
-    $stmt = $this->db->prepare('UPDATE tickets SET title = :title, contents = :contents WHERE id = :id');
-    $stmt->execute($ticket);
-    return $response->withRedirect("/tickets");
-});
-
+$app->put('/tickets/{id}', TicketsController::class . ':update');
 // 削除
-$app->delete('/tickets/{id}', function (Request $request, Response $response, array $args) {
-    $sql = 'SELECT * FROM tickets WHERE id = :id';
-    $stmt = $this->db->prepare($sql);
-    $stmt->execute(['id' => $args['id']]);
-    $ticket = $stmt->fetch();
-    if (!$ticket) {
-        return $response->withStatus(404)->write('not found');
-    }
-    $stmt = $this->db->prepare('DELETE FROM tickets WHERE id = :id');
-    $stmt->execute(['id' => $ticket['id']]);
-    return $response->withRedirect("/tickets");
+$app->delete('/tickets/{id}', TicketsController::class . ':delete');
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
 });
