212eb10cbfb60cd3e45aacdc76af1c9f8f1682b0
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-03-10 17:18:43 +0900

    Controllerを新規作成

--- src/classes/controllers/Controller.php
+++ src/classes/controllers/Controller.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace Classes\Controllers;
-
 use Psr\Container\ContainerInterface;
 use Slim\Views\PhpRenderer;
-
 abstract class Controller
 {
     /** @var \PDO */
     protected $db;
     /** @var PhpRenderer */
     protected $renderer;
-
     public function __construct(ContainerInterface $container)
     {
         $this->db = $container['db'];
